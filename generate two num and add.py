ed86e159d7a04b50e3ce1d12e4e1c0c3c0a88207
jayashree-jeyak <jayashreejeyak@gmail.com>
2025-01-28 21:44:49 +0530

    Add files via upload

---
+++ generate two num and add.py
@@ -0,0 +1,7 @@
+import random
+a=random.randint(1,100)
+b=random.randint(1,100)
+c=a+b
+print("THE FIRST NUMBER IS:",a)
+print("THE SECOND NUMBER IS:",b)
+print("THE SUM OF THE TWO GENERATED NUM IS:",c)
